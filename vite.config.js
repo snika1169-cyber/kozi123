4a461d8f80ece657e68496842a9299011f216f9e
Sonika <your-email@gmail.com>
2026-08-17 13:58:17 +0700

    Fix configuration files, routes, and styling setup

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-
+i// Remove or update lines like this:
+import shirtImg from '../assets/Shoe_photo/.png';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
